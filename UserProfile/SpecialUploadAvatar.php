b1e61284411270f584880a6a4e4c91906b74ad7c
Pierre Boutet <pierre.boutet@recommerce.com>
2015-11-02 22:29:22 +0100

    changement taille avatars

--- UserProfile/SpecialUploadAvatar.php
+++ UserProfile/SpecialUploadAvatar.php
@@ -388,10 +388,10 @@ class UploadAvatar extends UploadFromFile {
 			$stats->incStatField( 'user_image' );
 		}
 
-		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_l', 75 );
-		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_ml', 50 );
-		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_m', 30 );
-		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_s', 16 );
+		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_l', 300 );
+		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_ml', 100 );
+		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_m', 60 );
+		$this->createThumbnail( $this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $uid . '_s', 32 );
 
 		if ( $ext != 'jpg' ) {
 			if ( is_file( $this->avatarUploadDirectory . '/' . $wgAvatarKey . '_' . $uid . '_s.jpg' ) ) {
